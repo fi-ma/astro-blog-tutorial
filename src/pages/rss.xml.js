56ba03165ea997146cb4ccdeb14b15d0566783d6
fi-ma <filip.mares@gmx.at>
2023-07-03 13:01:17 +0200

    finalize tags implementation and add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+﻿import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://neon-cannoli-b6cc2d.netlify.app/',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
